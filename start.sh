61552cb6116d5f55a7c649ff55152f9df95faade
LazoYoung <idealproduction@naver.com>
2023-07-18 01:40:09 +0900

    Refactor start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 session=github-actions-book-market
-tmux attach -t $session || tmux new-session -d -s $session
+tmux has-session -t $session || tmux new-session -d -s $session
 tmux send-keys -t $session C-c
 tmux send-keys -t $session "bash" C-m
 tmux send-keys -t $session "cd $PWD" C-m
