09dabb059415fd24935291eb6f7ddd146107d98f
LazoYoung <idealproduction@naver.com>
2023-07-17 15:48:41 +0900

    Update env files

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ tmux has-session -t $session || tmux new-session -d -s $session
 tmux send-keys -t $session C-c
 tmux send-keys -t $session "bash" C-m
 tmux send-keys -t $session "cd $PWD" C-m
-tmux send-keys -t $session "echo $1 | sudo -S ./gradlew bootRun" C-m
+tmux send-keys -t $session "./gradlew bootRun" C-m
